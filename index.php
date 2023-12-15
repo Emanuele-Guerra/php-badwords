1343c424af69b6dff85d69c5cbb3b24b4095c936
Emanuele-Guerra <emaguerra1993@hotmail.it>
2023-12-15 17:32:06 +0100

    push

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php $phrase = "hell(o) world" ?>
+    <?php $img = "img/da.jpg" ?>
+</head>
+<body>
+    <div>
+        <h1><?php echo $nome ?></h1>
+        <img src="<?php $img ?>" alt="">
+    </div>
+</body>
+</html>
